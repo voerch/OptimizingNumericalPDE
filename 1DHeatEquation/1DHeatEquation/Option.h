4ece877bf0b78244d47571e945916cf12c1c2482
Mustafa Berke Erdis <mbe31@STUDENT.EECS.QMUL.AC.UK>
2019-07-22 18:45:53 +0100

    analytic bs

--- 1DHeatEquation/1DHeatEquation/Option.h
+++ 1DHeatEquation/1DHeatEquation/Option.h
@@ -2,6 +2,11 @@
 
 #include <algorithm>
 
+
+const double PI = 4.0 * atan(1.0);
+
+
+
 class VanillaOption 
 {
 
@@ -13,8 +18,25 @@ public:
 	double timeToExpiry;
 	double sigma;
 
+	
 	VanillaOption(double strike_, double interestRate_, double timeToExpiry_, double sigma_) :strike(strike_), interestRate(interestRate_), timeToExpiry(timeToExpiry_), sigma(sigma_) {};
 
+
+	virtual double PriceByBS(double S0) = 0;
+	double d_plus(double S0)
+	{
+		return (log(S0 / strike) + (interestRate + 0.5*pow(sigma, 2.0))*timeToExpiry) / (sigma*sqrt(timeToExpiry));
+	}
+
+	double d_minus(double S0)
+	{
+		return d_plus(S0) - sigma*sqrt(timeToExpiry);
+	}
+
+	double NormDist(double x)
+	{
+		return 0.5 * erfc(-x * sqrt(0.5));
+	}
 };
 
 class EurCall : public VanillaOption
@@ -25,19 +47,28 @@ public:
 		return std::max(sharePrice - strike, 0.0);
 	}
 
-	EurCall(double strike_, double interestRate_, double timeToExpiry_, double sigma_) : VanillaOption(strike_, interestRate_, timeToExpiry_, sigma_) {}
-
-
-};
-
-class EurPut : public VanillaOption
-{
-public:
-	double Payoff(double sharePrice)
+	double PriceByBS(double S0)
 	{
-		return std::max(strike - sharePrice , 0.0);
+		return S0*NormDist(d_plus(S0)) - strike*exp(-interestRate*timeToExpiry)*NormDist(d_minus(S0));
 	}
 
-	EurPut(double strike_, double interestRate_, double timeToExpiry_, double sigma_) : VanillaOption(strike_, interestRate_, timeToExpiry_, sigma_) {}
+	EurCall(double strike_, double interestRate_, double timeToExpiry_, double sigma_) : VanillaOption(strike_, interestRate_, timeToExpiry_, sigma_) {}
 
 };
+
+//class EurPut : public VanillaOption
+//{
+//public:
+//	double Payoff(double sharePrice)
+//	{
+//		return std::max(strike - sharePrice , 0.0);
+//	}
+//
+//	EurPut(double strike_, double interestRate_, double timeToExpiry_, double sigma_) : VanillaOption(strike_, interestRate_, timeToExpiry_, sigma_) {}
+//
+//};
+
+
+
+
+
--- 1DHeatEquation/1DHeatEquation/main.cpp
+++ 1DHeatEquation/1DHeatEquation/main.cpp
@@ -1,6 +1,8 @@
 #include "PDE.h"
 #include "FDM.h"
 #include "Option.h"
+#include <iostream>
+#include <chrono>
 
 int main()
 {
@@ -31,14 +33,27 @@ int main()
 	unsigned long N = 20;
 
 	VanillaOption* callOption = new EurCall(K, r, T, v);
-
 	BlackScholesPDE* bsPDE = new BlackScholesPDE(callOption);
+
+	auto start = std::chrono::high_resolution_clock::now();
 
 	ExplicitMethod euler(x_dom, J, t_dom, N, bsPDE);
 	euler.stepMarch();
 
+	auto finish = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = finish - start;
+	std::cout << "Explicit Method elapsed time: " << elapsed.count() << std::endl;
+	
+	auto begin = std::chrono::high_resolution_clock::now();
+
 	CrankNicholson cn(x_dom, J, t_dom, N, bsPDE);
 	cn.stepMarch();
+
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed1 = end - begin;
+	std::cout << "CN Method elapsed time: " << elapsed1.count() << std::endl;
+
+	std::cout << callOption->PriceByBS(1.0) << std::endl;
 
 	delete bsPDE;
 
